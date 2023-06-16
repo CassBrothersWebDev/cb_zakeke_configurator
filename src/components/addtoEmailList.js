b81bff016cd898f5c6c7eb00c6b3867934da431e
CassBrothersWebDev <136762388+CassBrothersWebDev@users.noreply.github.com>
2023-06-16 12:06:09 +1000

    Update addtoEmailList.js

--- src/components/addtoEmailList.js
+++ src/components/addtoEmailList.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export async function addEmail (user_email) {
     try {
       console.log(`${user_email} is to be added to mailing list`);
-      const response = await axios.post('https://cb-emailer-api.vercel.app/add-email', { email: user_email });
+      const response = await axios.post('https://cb-emailer-api-ochre.vercel.app/add-email', { email: user_email });
       console.log(response);
     } catch (error) {
       console.log(error);
